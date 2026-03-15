2af0d794f0de5367e3f5f99e1edc6ace00572a77
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-03-15 13:51:41 +0700

    login.register.authentication

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/products', require('./routes/products'));
 app.use('/api/v1/categories', require('./routes/categories'));
 app.use('/api/v1/roles', require('./routes/roles'));
+app.use('/api/v1/auth', require('./routes/auth'));
 
 mongoose.connect('mongodb://localhost:27017/NNPTUD-C3');
 mongoose.connection.on('connected',()=>{
